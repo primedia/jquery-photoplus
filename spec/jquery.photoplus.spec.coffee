88838f0b176c2aa7e475dd883de0040d33418a9f
Akil Brooks <akil.brooks@gmail.com>
2014-03-17 09:09:51 -0400

    [AB] [67484522] Make gallery browsing specs
    [AB] [67484522] Update specs

--- spec/jquery.photoplus.spec.coffee
+++ spec/jquery.photoplus.spec.coffee
@@ -1,9 +1,52 @@
 describe "photoplus", ['jquery', 'jasmine', 'jasmine-jquery', 'photoplus'], ($) ->
-  # $ = null
 
   beforeEach ->
     loadFixtures "../../../fixtures/photoplus.html"
 
+
   describe "initialize", ->
     it "should define the function", ->
       expect($().photoplus).toBeDefined()
+
+  describe "gallery browsing", ->
+    beforeEach ->
+      $(".photoplus").photoplus()
+      $('.result').trigger('mouseenter')
+
+    it "should set the image count", ->
+      expect( $(".scroll_image_counter").text() ).toEqual("1/4")
+
+    it "should update the image", (done) ->
+      imageUrl = "http://image.apartmentguide.com/imgr/0dc84d4fa24ecf6108b58af65ec22aa0/140-105?city=Decatur&property_name=The%20Conservatory%20At%20Druid%20Hills"
+      setTimeout =>
+        expect( $('img:first:visible').attr('src') ).toEqual(imageUrl)
+        done()
+      , 1000
+      $('.scrollingHotSpotRight').click()
+
+    it "should update the counter", (done) ->
+      setTimeout =>
+        expect( $(".scroll_image_counter").text() ).toEqual("2/4")
+        done()
+      , 1000
+      $('.scrollingHotSpotRight').click()
+
+    it "should continue from the beginning", (done) ->
+      pending()
+      clicks = 0
+      interval = (setInterval =>
+        clicks++
+        $('.scrollingHotSpotRight').click()
+        if clicks == 5
+          clearInterval(interval)
+          done()
+      , 500)
+      expect( $(".scroll_image_counter").text() ).toEqual("1/4")
+      # This actually is not a feature [yet]
+
+    it "should loop back to the end", (done) ->
+      pending()
+      setTimeout done, 1000
+      $('.scrollingHotSpotLeft').click()
+      expect( $(".scroll_image_counter").text() ).toEqual("4/4")
+      # This actually is not a feature [yet]
--- spec/jquery.photoplus.spec.js
+++ spec/jquery.photoplus.spec.js
@@ -3,9 +3,60 @@ describe("photoplus", ['jquery', 'jasmine', 'jasmine-jquery', 'photoplus'], func
   beforeEach(function() {
     return loadFixtures("../../../fixtures/photoplus.html");
   });
-  return describe("initialize", function() {
+  describe("initialize", function() {
     return it("should define the function", function() {
       return expect($().photoplus).toBeDefined();
+    });
+  });
+  return describe("gallery browsing", function() {
+    beforeEach(function() {
+      $(".photoplus").photoplus();
+      return $('.result').trigger('mouseenter');
+    });
+    it("should set the image count", function() {
+      return expect($(".scroll_image_counter").text()).toEqual("1/4");
+    });
+    it("should update the image", function(done) {
+      var imageUrl;
+      imageUrl = "http://image.apartmentguide.com/imgr/0dc84d4fa24ecf6108b58af65ec22aa0/140-105?city=Decatur&property_name=The%20Conservatory%20At%20Druid%20Hills";
+      setTimeout((function(_this) {
+        return function() {
+          return done();
+        };
+      })(this), 1000);
+      $('.scrollingHotSpotRight').click();
+      return expect($('img:first:visible').attr('src')).toEqual(imageUrl);
+    });
+    it("should update the counter", function(done) {
+      setTimeout((function(_this) {
+        return function() {
+          expect($(".scroll_image_counter").text()).toEqual("2/4");
+          return done();
+        };
+      })(this), 1000);
+      return $('.scrollingHotSpotRight').click();
+    });
+    it("should continue from the beginning", function(done) {
+      var clicks, interval;
+      pending();
+      clicks = 0;
+      interval = setInterval((function(_this) {
+        return function() {
+          clicks++;
+          $('.scrollingHotSpotRight').click();
+          if (clicks === 5) {
+            clearInterval(interval);
+            return done();
+          }
+        };
+      })(this), 500);
+      return expect($(".scroll_image_counter").text()).toEqual("1/4");
+    });
+    return it("should loop back to the end", function(done) {
+      pending();
+      setTimeout(done, 1000);
+      $('.scrollingHotSpotLeft').click();
+      return expect($(".scroll_image_counter").text()).toEqual("4/4");
     });
   });
 });
